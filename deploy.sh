49f61a2555f8507d3e3bcbab95cd42a139962e42
sladkkov <sladkkov@yandex.ru>
2023-10-22 22:40:12 +0400

    add codecov.yml

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "Stop container"
-docker stop case-lab-java-crm
-docker rm case-lab-java-crm
-docker image rm {dockerhub username}/{dockerhub repo name}
-echo "Pull image"
-docker pull {dockerhub username}/{dockerhub repo name}
-echo "Start frontend container"
-docker run -p 80:80 --name frontend -d {dockerhub username}/{dockerhub repo name}
-echo "Finish deploying!"
