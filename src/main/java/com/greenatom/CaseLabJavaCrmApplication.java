b902c2ce70d1b7f2dd96da0239aa4c4e8bc9c0dc
sladkkov <sladkkov@yandex.ru>
2023-10-24 22:20:26 +0400

    add codecov.yml

--- src/main/java/com/greenatom/CaseLabJavaCrmApplication.java
+++ src/main/java/com/greenatom/CaseLabJavaCrmApplication.java
@@ -1,13 +1,30 @@
 package com.greenatom;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class CaseLabJavaCrmApplication {
+    @Value("${spring.datasource.password}")
+    private static String password;
+    @Value("${spring.datasource.username}")
+    private static String username;
+    @Value("${spring.datasource.database}")
+    private static String database;
 
     public static void main(String[] args) {
         SpringApplication.run(CaseLabJavaCrmApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info(password);
+        log.info(username);
+        log.info(database);
     }
 
 }
